9d9f6f3b33a1d69a84553f88abdf1ebf3e8875c7
RobynGebbie <2127777@students.wits.ac.za>
2022-05-20 13:49:05 +0200

    Database Connection

--- index.js
+++ index.js
@@ -10,3 +10,39 @@ app.use('/public', express.static(__dirname + '/public'));
 const port = process.env.PORT || 3000
 app.listen(port)
 console.log('Express server running on port', port)
+
+const sql = require('mssql');
+const mysql = require('mysql');
+const dotenv = require('dotenv');
+dotenv.config();
+const { Connection, Request } = require("tedious");
+
+// Create connection to database
+const config = {
+  authentication: {
+    options: {
+      userName: process.env.DB_USER, // update me
+      password: process.env.DB_PASSWORD // update me
+    },
+    type: "default"
+  },
+  server: process.env.DB_HOST, // update me
+  options: {
+    database: process.env.DB_NAME, //update me
+    encrypt: true
+  }
+};
+
+const connection = new Connection(config);
+
+// Attempt to connect and execute queries if connection goes through
+connection.on("connect", err => {
+  if (err) {
+    console.error(err.message);
+  } else {
+    //executeStatement();
+  }
+});
+
+connection.connect();
+
