cb49c307163400244da2b29368507ace4aff8c75
Nick Warrener <untidyram@outlook.com>
2022-05-26 09:38:56 +0200

    Small changes to login screen UI and backend.
    Added title.
    Changed login system to use Promises. Adapted bootstrap simple form to make a login form. Did some refactoring.

--- src/public/login.js
+++ src/public/login.js
@@ -1,32 +1,48 @@
 'use strict'
+
 const request = new XMLHttpRequest()
 request.addEventListener('error', onError)
 
-document.getElementById('output').style.display = 'None'// switch this output off until button is clicked
-
-function loggingIn () {
-  // open the post request to the server with url of log
-  request.open('POST', '/log', true)
-  request.setRequestHeader('Content-type', 'application/json')
-  // get username and password
-  const username = document.getElementById('username').value
-  const password = document.getElementById('password').value
-  // send username and password to ther server via json
-  request.send(JSON.stringify({ usernameInput: username, passwordInput: password }))
-  // wait for server to respond back
-  request.addEventListener('load', receivedValue)
-  // set the output to be visible
-  document.getElementById('output').style.display = 'initial'// initial is on
+window.onload = function () {
+  document.getElementById('loginbutton').addEventListener('click', function (evt) {
+    evt.preventDefault()
+    // open the post request to the server with url of log
+    request.open('POST', '/log', true)
+    request.setRequestHeader('Content-type', 'application/json')
+    // get username and password
+    const username = document.getElementById('username').value
+    const password = document.getElementById('password').value
+    // send username and password to the server via json
+    request.send(JSON.stringify({ usernameInput: username, passwordInput: password }))
+    // wait for server to respond back
+    request.addEventListener('load', receivedValue)
+  })
 }
 
 function receivedValue () {
-  // parse the data recieved from server
+  // parse the data received from server
   const response = JSON.parse(this.responseText)
   // get the msg of the json (get the value of a field loggedInOrNot)
   const msg = response.loggedInOrNot
+  console.log(msg)
   // out put the value
   document.getElementById('output').innerHTML = msg
-};
+
+  if (msg === 'Username and password are invalid.') {
+    document.getElementById('password').className = 'form-control is-invalid'
+    document.getElementById('username').className = 'form-control is-invalid'
+  } else if (msg === 'Please input a valid password') {
+    document.getElementById('username').className = 'form-control'
+    document.getElementById('password').className = 'form-control is-invalid'
+  } else if (msg === 'Please input a valid username') {
+    document.getElementById('username').className = 'form-control is-invalid'
+    document.getElementById('password').className = 'form-control'
+  } else {
+    // Let's reset everything
+    document.getElementById('username').className = 'form-control'
+    document.getElementById('password').className = 'form-control'
+  }
+}
 
 function onError () {
   // Let's tell the user that something wrong happened.
