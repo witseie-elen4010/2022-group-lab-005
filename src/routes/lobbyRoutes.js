001075a6ea483564f3225000d650a3da223435c1
Nick Warrener <untidyram@outlook.com>
2022-05-26 16:15:41 +0200

    Beginnings of the multiplayer communication implementation. Very broken.

--- src/routes/lobbyRoutes.js
+++ src/routes/lobbyRoutes.js
@@ -1,3 +1,4 @@
+// NO TOUCHY!!! const ioFmIndex = require('../index')
 const path = require('path')
 const bodyParser = require('body-parser')
 const express = require('express')
@@ -6,16 +7,42 @@ const jsonParser = bodyParser.json()
 const lobbyRouter = express.Router()
 
 const { createGame, prevGameID } = require('../services/lobby.js')
+// NO TOUCHY!! const { Router } = require('express')
+
+// NO TOUCHY!!const { getNumPlayers } = require('../services/soc')
 
 lobbyRouter.post('/', async function (req, res) { // ?
-    res.sendFile(path.join(__dirname, '../views', 'lobby.html'))
+  res.sendFile(path.join(__dirname, '../views', 'lobby.html'))
 })
 
+// NO TOUCHY!!
+/* lobbyRouter.get('/', async function (req, res) { // ?
+  res.sendFile(path.join(__dirname, '../views', 'lobby.html'))
+}) */
+
+// NO TOUCHY!!
+/* lobbyRouter.post('/join', async function (req, res) { // ?
+  if (getNumPlayers() === undefined) {
+    // No body is in the room, so lets join it and wait for the others.
+    console.log('No players in the room')
+  } else {
+    console.log(getNumPlayers())
+  }
+}) */
+
 lobbyRouter.post('/create', async function (req, res) { // ?
-    const prevResult = await prevGameID()
-    const newID = parseInt(prevResult) + 1
-    const result = await createGame(req.body.gameModeInput, newID)
-    res.sendFile(path.join(__dirname, '../views', 'game.html'))
+  /* //NO TOUCHY!!
+  if (getNumPlayers() === undefined) {
+    // No body is in the room, so lets join it and wait for the others.
+    console.log('No players in the room')
+  } else {
+    console.log(getNumPlayers())
+  } */
+
+  const prevResult = await prevGameID()
+  const newID = parseInt(prevResult) + 1
+  const result = await createGame(req.body.gameModeInput, newID)
+  res.sendFile(path.join(__dirname, '../views', 'game.html'))
 })
 
 module.exports = lobbyRouter
