1be675199813635ef1a5d0b8b3edb04884615a01
RobynGebbie <2127777@students.wits.ac.za>
2022-05-31 15:42:25 +0200

    Log Background in Database

--- src/public/changeBackground.js
+++ src/public/changeBackground.js
@@ -1,18 +1,24 @@
 'use strict'
 const background = document.querySelector('html, .background');
-// when user selects the dark mode button send dark mode is true to server
+
 const forestButton = document.getElementById('forestButton')
 forestButton.addEventListener('click', function () {
     background.style.setProperty('background-image', 'url("/src/public/Forest.jpg")');
+    const back = 'Forest';
+    sendBackgroundToServer(back);
 }, false)
 
-// when user selects the light mode button send dark mode is false to server
+
 const sunBeachButton = document.getElementById('sunBeachButton')
 sunBeachButton.addEventListener('click', function () {
     background.style.setProperty('background-image', 'url("/src/public/Beach.jpg")');
+    const back = 'Beach';
+    sendBackgroundToServer(back);
 }, false)
 
 const mountainsButton = document.getElementById('mountainsButton')
 mountainsButton.addEventListener('click', function () {
     background.style.setProperty('background-image', 'url("/src/public/Mountain.jpg")');
+    const back = 'Mountains';
+    sendBackgroundToServer(back);
 }, false)
