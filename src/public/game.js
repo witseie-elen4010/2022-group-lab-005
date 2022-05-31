f5111b4975f09ef8e003b89be7ba3532b564be3c
Nick Warrener <untidyram@outlook.com>
2022-05-31 16:20:25 +0200

    Integrating with Jesse's code.

--- src/public/game.js
+++ src/public/game.js
@@ -1,5 +1,5 @@
-import { io } from "https://cdn.socket.io/4.4.1/socket.io.esm.min.js";
-const socket = io();
+//import { io } from "https://cdn.socket.io/4.4.1/socket.io.esm.min.js";
+//const socket = io();
 
 let letterArray = [
   ["", "", "", "", "", ""],
