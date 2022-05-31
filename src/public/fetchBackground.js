e4100674bb0731fad0353060f641c4c8f78b315a
RobynGebbie <2127777@students.wits.ac.za>
2022-05-31 16:50:30 +0200

    Conflict fix

--- src/public/fetchBackground.js
+++ src/public/fetchBackground.js
@@ -12,21 +12,26 @@ fetch('/game/api/BackgroundData')
 
   .then(function (data) {
     // set background setting to data from api
+    // Get HTML head element
 
     const background = document.querySelector('html, .background');
-    
+
     if (data === "Mountains") {
-        document.body.style.backgroundImage='url("/src/public/Mountain.jpg")';
+     // document.body.style.backgroundImage = 'url("/src/public/Mountain.jpg")';
+     background.style.setProperty('background-image', 'url("/src/public/Mountain.jpg")');
     } else {
-        if (data === "Forest") {
-            document.body.style.backgroundImage='url("/src/public/Forest.jpg")';
+      if (data === "Forest") {
+       // document.body.style.backgroundImage = 'url("/src/public/Forest.jpg")';
+       background.style.setProperty('background-image', 'url("/src/public/Forest.jpg")');
+      }
+      else {
+        if (data === "Beach") {
+        //  document.body.style.backgroundImage = 'url("/src/public/Beach.jpg")';
+        background.style.setProperty('background-image', 'url("/src/public/Beach.jpg")');
         }
-        else {
-            if (data === "Beach") {
-                document.body.style.backgroundImage='url("/src/public/Beach.jpg")';
-            }
-        }
+      }
     }
+    var head = document.getElementsByTagName('HEAD')[0];
 
   })
   .catch(error => document.write(`Fetch failed: ${error}`))
