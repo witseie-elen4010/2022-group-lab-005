bf688f8a93c10ddd27a6461dea40fbbea710db7f
Roger <rdemellokoch@gmail.com>
2022-05-31 13:48:00 +0200

    Changed getUserStats
    getUserStats returns a list of the number of guesses in each game where the user won

--- src/services/matchHistory.js
+++ src/services/matchHistory.js
@@ -3,56 +3,53 @@ const { resolve } = require('path')
 const { get } = require('./poolManagement')
 
 async function getUserGames(userID) {
-    // Returns the placement and GameID of any game the user took part in
-    const sqlCode = `SELECT GameID, Placement FROM [dbo].[UserGame] WHERE UserID='${userID}';`
-    return new Promise((resolve, reject) => {
-      get('default').then(
-        (pool) => pool.request().query(sqlCode).then(
-          (result) => {
-            resolve(result)
-          }
-        ).catch(reject)
+  // Returns the placement and GameID of any game the user took part in
+  const sqlCode = `SELECT GameID, Placement FROM [dbo].[UserGame] WHERE UserID='${userID}';`
+  return new Promise((resolve, reject) => {
+    get('default').then(
+      (pool) => pool.request().query(sqlCode).then(
+        (result) => {
+          resolve(result)
+        }
       ).catch(reject)
-    })
+    ).catch(reject)
+  })
 }
 
 async function getUserStats(gamesList, userID) {
-    // Returns all guesses made in games the user won
-    let sqlCode = `SELECT GameID FROM [dbo].[UserGame] WHERE UserID='${userID}'`
-    const allWins = []
-    for(let i = 0; i < Object.keys(gamesList).length; i++){
-        if(gamesList.recordset[i].Placement === 1){
-            allWins.push(gamesList.recordset[i].GameID)
+  // Returns all guesses made in games the user won
+  let sqlCode = `SELECT COUNT(*) GameID, `
+  const allWins = []
+  let fristAddition = true
+  for (let i = 0; i < Object.keys(gamesList).length; i++) {
+    if (gamesList.recordset[i].Placement === 1) {
+      if(fristAddition === true){
+        sqlCode += `SUM(CASE WHEN GameID = '${gamesList.recordset[i].GameID}' AND UserID = '${userID}' THEN 1 ELSE 0 END)`
+        fristAddition = false
+      }
+      else{
+        sqlCode += `, SUM(CASE WHEN GameID = '${gamesList.recordset[i].GameID}' AND UserID = '${userID}' THEN 1 ELSE 0 END)`
+      }
+    }
+  }
+  if (fristAddition === false) {
+    sqlCode += ` FROM [dbo].[Guess] GROUP BY GameID;`
+  }
+  else {
+    sqlCode += `FROM [dbo].[Guess] WHERE UserID='0';`
+  }
+  return new Promise((resolve, reject) => {
+    get('default').then(
+      (pool) => pool.request().query(sqlCode).then(
+        (result) => {
+          resolve(result)
         }
-    }
-    let fristAddition = true
-    for(let i = 0; i < allWins.length; i++){
-        if(fristAddition === true){
-            sqlCode += ` AND (GameID='${allWins[i]}'`
-            fristAddition = false
-        }
-        else{
-            sqlCode += ` OR GameID='${allWins[i]}'`
-        }
-    }
-    if(fristAddition === false){
-        sqlCode += `);`
-    }
-    else{
-        sqlCode += ` AND GameID='0';`
-    }
-    return new Promise((resolve, reject) => {
-      get('default').then(
-        (pool) => pool.request().query(sqlCode).then(
-          (result) => {
-            resolve(result)
-          }
-        ).catch(reject)
       ).catch(reject)
-    })
+    ).catch(reject)
+  })
 }
 
 module.exports = {
-    getUserGames:getUserGames,
-    getUserStats:getUserStats
+  getUserGames: getUserGames,
+  getUserStats: getUserStats
 }
