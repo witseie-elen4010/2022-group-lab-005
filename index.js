1249b08ac54a8fcb8a8328a6c3373c6c713f60ec
Nick Warrener <untidyram@outlook.com>
2022-05-31 09:55:40 +0200

    Working on sockets. index.js uses a toggle to enable the code I've added. Setting line 4 to false will run the new code which sets up the sockets.

--- index.js
+++ index.js
@@ -1,45 +1,69 @@
 'use strict'
 require('dotenv').config()
-// const { Connection, Request } = require('tedious')
-const path = require('path')
-const bodyParser = require('body-parser')
-const express = require('express')
-const app = express()
-const mainRouter = require('./src/mainRoutes')
-//NO TOUCHY!! const { socketConnection } = require('./services/soc')
 
-// app.use('/', mainRouter)
+const stdConfig = true
 
-app.use(express.static('public'))
-// app.use('/public', express.static(__dirname + '/public'))
-app.use('/src/public', express.static(__dirname + '/src/public'))
-//NO TOUCHY!! app.use('/node_modules', express.static(path.join(__dirname, 'node_modules/')))
-app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
-app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
-app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')))
-app.use('/services', express.static(path.join(__dirname, 'src/services')))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+// If stdConfig is true, the code will behave how it used to.
+// If it is false, then the code will behave how it used to but with
+// the addition of sockets.
+if (stdConfig === true) {
+  // const { Connection, Request } = require('tedious')
+  const path = require('path')
+  const bodyParser = require('body-parser')
+  const express = require('express')
+  const app = express()
+  const mainRouter = require('./src/mainRoutes')
 
-//NO TOUCHY!!
-/*
-const http = require('http')
-const server = http.createServer(app)
-const { Server } = require('socket.io')
-const io = new Server(server)
+  // app.use('/', mainRouter)
 
-io.on('hi', (socket) => {
-  console.log('User wants to join a game')
-})
+  app.use(express.static('public'))
+  // app.use('/public', express.static(__dirname + '/public'))
+  app.use('/src/public', express.static(__dirname + '/src/public'))
+  app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
+  app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
+  app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')))
+  app.use('/services', express.static(path.join(__dirname, 'src/services')))
+  app.use(bodyParser.json())
+  app.use(bodyParser.urlencoded({ extended: true }))
 
-server.listen(25565, () => {
-  console.log('Socket.io listening on 25565')
-})
-*/
+  app.use(mainRouter)
+  // app.use('/public', express.static(__dirname + '/public'))
 
-app.use(mainRouter)
-// app.use('/public', express.static(__dirname + '/public'))
+  const port = process.env.PORT || 3000
+  app.listen(port)
+  console.log('Express server running on port', port)
+} else {
+  const path = require('path')
+  const bodyParser = require('body-parser')
+  const express = require('express')
+  const app = express()
 
-const port = process.env.PORT || 3000
-app.listen(port)
-console.log('Express server running on port', port)
+  const http = require('http')
+  const server = http.createServer(app)
+  const { Server } = require('socket.io')
+  const io = new Server(server)
+
+  const mainRouter = require('./src/mainRoutes')
+  // app.use('/', mainRouter)
+
+  app.use(express.static('public'))
+  app.use('/src/public', express.static(__dirname + '/src/public'))
+  app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
+  app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
+  app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')))
+  app.use('/services', express.static(path.join(__dirname, 'src/services')))
+  app.use(bodyParser.json())
+  app.use(bodyParser.urlencoded({ extended: true }))
+
+  app.use(mainRouter)
+  // app.use('/public', express.static(__dirname + '/public'))
+
+  io.on('connection', (socket) => {
+    console.log('a user connected')
+  })
+
+  const port = process.env.PORT || 3000
+  server.listen(port, () => {
+    console.log(`listening on *:${port}`)
+  })
+}
