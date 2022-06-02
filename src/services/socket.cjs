5d58725f148ad0d24c45e7aae4dc734062ea43b2
Nick Warrener <untidyram@outlook.com>
2022-06-02 10:38:50 +0200

    Added support for multiple game instances at the same time :)

--- src/services/socket.cjs
+++ src/services/socket.cjs
@@ -6,39 +6,70 @@ module.exports = function (io) {
   io.on('connection', (socket) => {
     socket.on('send_guess', function (letterArray, currentWordIndex, colorArray, currentWordCheck, allLettersColorsArray) {
       const currentWordArray = ['H', 'E', 'L', 'L', 'O']
-
       const [letterArr, currWordIndex, colorArr, currWordCheck, allLettersColorsArr, didTheyWin] = testWord(letterArray, currentWordIndex, colorArray, currentWordCheck, allLettersColorsArray, currentWordArray)
 
-      socket.emit('update_player_screen', letterArr, currWordIndex, colorArr, currWordCheck, allLettersColorsArr, didTheyWin)
-      socket.broadcast.emit('update_opponent_colors', colorArr, didTheyWin, socket.playerName)
+      socket.emit('update_player_screen', letterArr, currWordIndex, colorArr, currWordCheck, allLettersColorsArr, didTheyWin) // These values get sent back to the sender.
+      socket.broadcast.to(socket.data.roomID).emit('update_opponent_colors', colorArr, didTheyWin, socket.data.playerName) // These values get broadcast to everyone except the sender.
     })
-
-    if (io.engine.clientsCount === 2) {
-      io.emit('game_can_start')
-    }
 
     socket.on('game_over', () => {
       socket.disconnect()
     })
   })
 
-  // When a new player attempts to join the server.
-  // TODO CHECK THAT THE SAME USER ISN'T ALREADY IN THE GAME!!
+  // This is called a middleware. Its a type of function that is run for every incoming connection.
+  // In this case, it is being used for authentication. If the code is happy with the details that the
+  // incoming connection specified, this code will add that connection to the specific room for the game.
+  // See https://socket.io/docs/v4/middlewares/ for details.
   io.use((socket, next) => {
-    const playerName = socket.handshake.auth.playerName
-    if (!playerName) {
-      return next(new Error('invalid_playername'))
-    } else {
-      // Add a listener for when a *connected* player leaves the server.
-      socket.on('disconnect', () => {
-        console.log(`${socket.playerName} has disconnected`)
-      })
+    // When the a connection is attempted, the client sends an object that contains the player's name and a string called sessionInfo. This string is of the format:
+    // x....xy where all the xs form the roomID and y is the number of players that are going to be in the game.
 
-      console.log(`${playerName} has joined the game`)
-      // We add a 'playerName' attribute *to* the socket object so we can
-      // use that name later on.
-      socket.playerName = playerName
-      next()
+    // We extract the gameID from the sessionInfo string.
+    const gameID = socket.handshake.auth.sessionInfo.substring(0, socket.handshake.auth.sessionInfo.length - 1)
+    // We extract the number of players from the sessionInfo string.
+    const numPlayers = socket.handshake.auth.sessionInfo.substring(socket.handshake.auth.sessionInfo.length - 1)
+    // We get the player's name.
+    const playerName = socket.handshake.auth.playerName
+
+    // Here is where we validate the session info.
+    // We make sure that the number of players is actually a number.
+    // We make sure that the number of players is between (inclusive) 2 and HOW MANY PLAYERS??
+    // We make sure that the gameID is valid (i.e. is of the correct form and has no invalid characters)
+    // TODO: Validate the gameID. If we use UUID for the gameID, then we can just use a pre-made validator.
+    if (numPlayers <= 6 && numPlayers > 1 && !isNaN(numPlayers)) {
+      if (parseInt(io.sockets.adapter.rooms.get(gameID).size) >= parseInt(numPlayers)) {
+        return next(new Error('game_already_running'))
+      } else {
+        // Add the player to the room specified by gameID.
+        socket.join(gameID)
+        console.log(`${playerName} has joined ${gameID}`)
+
+        // We add a 'playerName' attribute *to* the socket object so we can
+        // use that name later on.
+        socket.data.playerName = playerName
+        socket.data.roomID = gameID
+        socket.data.numPlayers = numPlayers
+
+        if (parseInt(io.sockets.adapter.rooms.get(socket.data.roomID).size) === parseInt(socket.data.numPlayers)) {
+          console.log(`All ${socket.data.numPlayers} players have joined ${socket.data.roomID}, starting the game.`)
+          // I think there's a better way to do this (only using one event) but I couldn't get anything to work
+          // other than this. This first line broadcasts the 'game_can_start' event to all the sockets in the room
+          // except to the sender. The second line sends the 'game_can_start' event to the sender.
+          socket.to(socket.data.roomID).emit('game_can_start')
+          socket.emit('game_can_start')
+        }
+
+        // Add a listener for when a connected socket leaves the server.
+        socket.on('disconnect', () => {
+          console.log(`${socket.data.playerName} has disconnected`)
+        })
+
+        // This must be here or else the connection will hang until it times out. Its part of the middleware stuff.
+        next()
+      }
+    } else {
+      return next(new Error('invalid_game_id'))
     }
   })
 }
