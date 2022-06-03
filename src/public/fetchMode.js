2527af8cad47501e892744a6c994e4afa9399e25
RobynGebbie <2127777@students.wits.ac.za>
2022-06-03 22:39:33 +0200

    Dark Mode Immediate Update

--- src/public/fetchMode.js
+++ src/public/fetchMode.js
@@ -1,5 +1,5 @@
 'use strict'
-
+//the issue is that fetch only happens in home page, the update is not instant
 // fetch data from api
 fetch('/user/api/DarkModeData')
 
@@ -11,36 +11,10 @@ fetch('/user/api/DarkModeData')
   })
 
   .then(function (data) {
+
     // set dark mode setting to data from api
     const darkMode = data
-    let textColour = 'black'
-    let backColour = 'white'
-    let buttonColour = 'grey'
-    const dark = document.querySelectorAll('.dark-mode')
-    document.style = ".dark-mode";
-    //  set current current scheme variables
-    if (darkMode === 'true') {
-      textColour = 'blue'
-      backColour = 'black'
-      buttonColour = 'white'
-      document.body.classList.toggle("dark-mode");
-    } else {
-      textColour = 'black'
-      backColour = 'white'
-      buttonColour = 'grey'
-    }
+    sessionStorage.setItem("mode", darkMode);
 
-    const settingsButton = document.getElementById('settingsButton')
-
-    // set colour scheme of this page
-    // document.body.style.color = textColour
-    // document.body.style.backgroundColor = backColour
-    // settingsButton.style.backgroundColor = buttonColour
-    // settingsButton.style.color = textColour
-
-    // // set current current scheme variables
-    // sessionStorage.setItem('textColour', textColour)
-    // sessionStorage.setItem('backColour', backColour)
-    // sessionStorage.setItem('buttonColour', buttonColour)
   })
   .catch(error => document.write(`Fetch failed: ${error}`))
