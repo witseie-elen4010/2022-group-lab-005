8dae59ddc3c695348d56e7d78b48c141b0d5a586
Nick Warrener <untidyram@outlook.com>
2022-06-03 15:20:14 +0200

    Fixed a bug where games that had just started were not removed from the open games table in the lobby page.

--- src/public/lobby.js
+++ src/public/lobby.js
@@ -1,6 +1,7 @@
 'use strict'
 import { io } from 'https://cdn.socket.io/4.4.1/socket.io.esm.min.js'
 const socket = io('/rooms')
+
 // Try establish a connection with the server.
 socket.connect()
 
@@ -8,14 +9,20 @@ socket.on('update_game_list', (openGames) => {
 
     let table = document.getElementById('gameInfo').getElementsByTagName('tbody')[0]
 
-    for (let i = 0; i < openGames.length; i++) {
-        let newRow = table.insertRow(i)
-        let gameName = newRow.insertCell(0)
-        let availPlayers = newRow.insertCell(1)
+    if (openGames.length !== 0) {
 
-        gameName.innerHTML = openGames[i].roomName
-        availPlayers.innerHTML = openGames[i].availSlots
+        // Add the new open games to the table.
+        for (let i = 0; i < openGames.length; i++) {
+            let newRow = table.insertRow(i)
+            let gameName = newRow.insertCell(0)
+            let availPlayers = newRow.insertCell(1)
+
+            gameName.innerHTML = openGames[i].roomName
+            availPlayers.innerHTML = openGames[i].availSlots
+        }
+    } else {
+        // There are no open games, so we must clear the table.
+        const emptyTblBdy = document.createElement('tbody')
+        table.parentNode.replaceChild(emptyTblBdy, table)
     }
-
-    //document.getElementById('openGames').innerHTML = openGames
 })
