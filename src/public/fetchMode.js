7971182b73bee15eb649c5f2b87637b073d9f919
Nick Warrener <untidyram@outlook.com>
2022-06-04 09:46:53 +0200

    Update fetchMode.js
    Fixed formatting :)

--- src/public/fetchMode.js
+++ src/public/fetchMode.js
@@ -1,20 +1,13 @@
 'use strict'
 //the issue is that fetch only happens in home page, the update is not instant
 // fetch data from api
-fetch('/user/api/DarkModeData')
-
-  .then(response => {
+fetch('/user/api/DarkModeData') .then(response => {
     if (!response.ok) {
       throw new Error(`HTTP error: ${response.status}`)
     }
     return response.text()
-  })
-
-  .then(function (data) {
-
+  }).then(function (data) {
     // set dark mode setting to data from api
     const darkMode = data
     sessionStorage.setItem("mode", darkMode);
-
-  })
-  .catch(error => document.write(`Fetch failed: ${error}`))
+  }).catch(error => document.write(`Fetch failed: ${error}`))
