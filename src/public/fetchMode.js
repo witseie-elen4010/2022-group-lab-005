fcdfa43159dda45dd52ad5d8009c0d1b084e9b3e
RobynGebbie <2127777@students.wits.ac.za>
2022-06-07 12:28:01 +0200

    Username Input Into Settings

--- src/public/fetchMode.js
+++ src/public/fetchMode.js
@@ -1,25 +1,41 @@
 'use strict'
 
+const request = new XMLHttpRequest()
 
 $(function () {
   //const fake = createFakeUser()
   //document.cookie = fake
   checkUser(document.cookie).then(
     (result) => {
-      if(result === false){
+      if (result === false) {
         window.location.href = "/login"
       }
     }
   ).catch()
 })
+getMode()
+function getMode() {
 
-function getMode () {
   request.open('GET', '/user/api/DarkModeData', true)
+  request.setRequestHeader('Content-type', 'application/json')
+  const username = getFromCookie('username', document.cookie)
+  request.send(JSON.stringify({ usernameInput: username }))
   request.addEventListener('load', setMode)
-  request.send()
 }
 
-function setMode () {
+function setMode() {
   const response = JSON.parse(this.responseText)
-  sessionStorage.setItem('mode', response)
+  console.log(response)
+
+  const darkMode = response
+
+  //  set current current scheme
+  if (darkMode === 'true') {
+    document.body.classList.remove('light-mode')
+    document.body.classList.add('dark-mode')
+  } else {
+    document.body.classList.remove('dark-mode')
+    document.body.classList.add('light-mode')
+  }
+  //sessionStorage.setItem('mode', response)
 }
