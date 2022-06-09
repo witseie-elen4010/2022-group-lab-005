6989267c47a675ceab747abf4e696971147330d8
Ray-R4y <88461856+Ray-R4y@users.noreply.github.com>
2022-06-09 20:08:10 +0200

    update input validation in server code
    moved cheking input validation into its own seperate function

--- src/services/loginDb.cjs
+++ src/services/loginDb.cjs
@@ -5,41 +5,33 @@ const { resolve } = require('path')
 async function LogIn (username, password) {
   const sqlCode = `SELECT Password FROM Users WHERE Username = '${username}'`
   return new Promise((resolve, reject) => {
-    // validate username input, password does not need to be validated due to base64 format from encryption
-    if (username === '' & password === '') {
-      resolve('Please input a username and password')
-    } else if (username === '') {
-      resolve('Please input a username')
-    } else if (password === '') {
-      resolve('Please input a password')
-    }
-
-    if (/^[a-zA-Z0-9]+$/.test(username) === false) {
-      resolve('Please input a valid username')
-    }
-
-    get('default').then(
-      (pool) => pool.request().query(sqlCode).then(
-        (result) => {
-          const list = JSON.stringify(result.recordset[0])
-          try {
-            if (list !== undefined) { // If this is true, then the username does not exist.
-              const obj = JSON.parse(list)
-              if (obj.Password === password) {
-                resolve('User is now logged in')
+    const validInput = validateInput(username, password)
+    if (validInput === 'valid') {
+      get('default').then(
+        (pool) => pool.request().query(sqlCode).then(
+          (result) => {
+            const list = JSON.stringify(result.recordset[0])
+            try {
+              if (list !== undefined) { // If this is true, then the username does not exist.
+                const obj = JSON.parse(list)
+                if (obj.Password === password) {
+                  resolve('User is now logged in')
+                } else {
+                  resolve('Check username and password.')
+                }
               } else {
-                resolve('Check username and password.')
+                resolve('Account does not exist.')
               }
-            } else {
-              resolve('Account does not exist.')
+            } catch (err) {
+              console.log(err)
+              reject(err)
             }
-          } catch (err) {
-            console.log(err)
-            reject(err)
           }
-        }
+        ).catch(reject)
       ).catch(reject)
-    ).catch(reject)
+    } else {
+      resolve(validInput)
+    }
   })
 }
 
@@ -50,43 +42,36 @@ async function registerUser (username, password) {
   INSERT INTO Settings (Username, Background, isDarkmode)
   VALUES ('${username}', 'None', 'false');`
   return new Promise((resolve, reject) => {
-    if (username === '' & password === '') {
-      resolve('Please input a username and password')
-    } else if (username === '') {
-      resolve('Please input a username')
-    } else if (password === '') {
-      resolve('Please input a password')
-    }
-
-    if (/^[a-zA-Z0-9]+$/.test(username) === false) {
-      resolve('Please input a valid username')
-    }
-
-    get('default').then(
-      (pool) => pool.request().query(sqlCodeCheckUserExist).then( // first query to see if the inputed username exists
-        (result) => {
-          const list = JSON.stringify(result.recordset[0])
-          try {
-            if (list === undefined) { // undefined list shows that username does not exist
-              get('default').then(
-                (pool) => pool.request().query(sqlCode).then(
-                  () => {
-                    resolve()
-                  }
+    const validInput = validateInput(username, password)
+    if (validInput === 'valid') {
+      get('default').then(
+        (pool) => pool.request().query(sqlCodeCheckUserExist).then( // first query to see if the inputed username exists
+          (result) => {
+            const list = JSON.stringify(result.recordset[0])
+            try {
+              if (list === undefined) { // undefined list shows that username does not exist
+                get('default').then(
+                  (pool) => pool.request().query(sqlCode).then(
+                    () => {
+                      resolve()
+                    }
+                  ).catch(reject)
                 ).catch(reject)
-              ).catch(reject)
 
-              resolve('Registration completed')
-            } else {
-              resolve('Account exists already.')
+                resolve('Registration completed')
+              } else {
+                resolve('Account exists already.')
+              }
+            } catch (err) {
+              console.log(err)
+              reject(err)
             }
-          } catch (err) {
-            console.log(err)
-            reject(err)
           }
-        }
+        ).catch(reject)
       ).catch(reject)
-    ).catch(reject)
+    } else {
+      resolve(validInput)
+    }
   })
 }
 
@@ -103,6 +88,21 @@ async function getUser (username) {
       ).catch(reject)
     ).catch(reject)
   })
+}
+
+function validateInput (username, password) {
+  // validate username input, password does not need to be validated due to hashing
+  if (username === '' & password === '') {
+    return 'Please input a username and password'
+  } else if (username === '') {
+    return 'Please input a username'
+  } else if (password === '') {
+    return 'Please input a password'
+  } else if (/^[a-zA-Z0-9]+$/.test(username) === false) {
+    return 'Please input a valid username'
+  } else {
+    return 'valid'
+  }
 }
 
 module.exports = { LogIn, registerUser, getUser }
