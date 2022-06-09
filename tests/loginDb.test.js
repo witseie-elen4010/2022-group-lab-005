04bfc8bfafd9f95fe5dc3156d005ce1ccee95c98
Ray-R4y <88461856+Ray-R4y@users.noreply.github.com>
2022-06-09 21:20:49 +0200

    Updated testing to fit database and changes
    Tests updated that is working to new code and database design

--- tests/loginDb.test.js
+++ tests/loginDb.test.js
@@ -1,13 +1,5 @@
 'use strict'
-const { LogIn, registerUser } = require('../src/services/loginDb')
-
-beforeEach(() => {
-  jest.useFakeTimers()
-})
-
-afterEach(() => {
-
-})
+const { LogIn, registerUser, validateInput } = require('../src/services/loginDb')
 // original format
 // test('user login if username input empty and password is empty', () => {
 //   LogIn('a', '').then(data => {
@@ -15,65 +7,57 @@ afterEach(() => {
 //   })
 // })
 
-test('user login if username input empty and password is empty', async () => {
-  const data = await LogIn('', '')
-  await expect(data).toBe('Please input a username and password')
+test('user input if username input empty and password is empty', () => {
+  const data = validateInput('', '')
+  expect(data).toBe('Please input a username and password')
 })
 
-test('user login if username input empty', async () => {
-  const data = await LogIn('', 'password')
-  await expect(data).toBe('Please input a username')
+test('user input if username input empty', async () => {
+  const data = validateInput('', 'password')
+  expect(data).toBe('Please input a username')
 })
 
-test('user login if password input empty', async () => {
-  const data = await LogIn('user', '')
-  await expect(data).toBe('Please input a password')
+test('user input if password input empty', async () => {
+  const data = validateInput('user', '')
+  expect(data).toBe('Please input a password')
 })
 
-test('user login if username is invalid', async () => {
-  const data = await LogIn('@dmin', 'password')
-  await expect(data).toBe('Please input a valid username')
+test('user input if username is invalid', () => {
+  const data = validateInput('@dmin', 'password')
+  expect(data).toBe('Please input a valid username')
 })
 
-// test('user login if username does not exist', async() => {
-//   const data = await LogIn('dadadadada', 'wrongpassword')
-//   await expect(data).toBe('Account does not exist.')
-// })
-// jest.setTimeout(10000)
+test('user input if everthing is valid', () => {
+  const data = validateInput('winner', 'fbe789cb41b2fc32df9b89cacff9830d85ac62de09ca48395ebcef69e069160a')
+  expect(data).toBe('valid')
+})
 
-// //winner is a user in the database with password： password
-// test('user login if password is incorrect', async() => {
-//   const data = await LogIn('winner', 'wrongpassword')
-//   await expect(data).toBe('Check username and password.')
-// })
-// jest.setTimeout(10000)
+test('user login if username does not exist', async () => {
+  const data = await LogIn('dadadadada', 'wrongpassword')
+  await expect(data).toBe('Account does not exist.')
+})
+jest.setTimeout(10000)
 
-// test('user login is sucessful', async() => {
-//   const data = await LogIn('winner', 'password')
-//   await expect(data).toBe('User is now logged in')
-// })
+// winner is a user in the database with password： password
+test('user login if password is incorrect', async () => {
+  const data = await LogIn('winner', 'wrongpassword')
+  await expect(data).toBe('Check username and password.')
+})
+jest.setTimeout(10000)
+
+test('user login is sucessful', async () => {
+  const data = await LogIn('winner', 'fbe789cb41b2fc32df9b89cacff9830d85ac62de09ca48395ebcef69e069160a')
+  await expect(data).toBe('User is now logged in')
+})
+jest.setTimeout(10000)
+
 // jest.setTimeout(10000)
 // code above is for Login function
 // ---------------------------------------
 // ---------------------------------------
 // code below is for registeration function
-
-test('user login if username input empty and password is empty', async () => {
-  const data = await registerUser('', '')
-  await expect(data).toBe('Please input a username and password')
+test('user registration if username exist already', async () => {
+  const data = await registerUser('winner', 'wrongpassword')
+  await expect(data).toBe('Account exists already.')
 })
-
-test('user login if username input empty', async () => {
-  const data = await registerUser('', 'password')
-  await expect(data).toBe('Please input a username')
-})
-
-test('user login if password input empty', async () => {
-  const data = await registerUser('user', '')
-  await expect(data).toBe('Please input a password')
-})
-
-test('user login if username is invalid', async () => {
-  const data = await registerUser('@dmin', 'password')
-  await expect(data).toBe('Please input a valid username')
-})
+jest.setTimeout(10000)
