5fa550bf8da9a9038144a37caeb458d654c0dd8b
RobynGebbie <2127777@students.wits.ac.za>
2022-06-09 11:42:24 +0200

    Some Small Visual Updates

--- src/services/settings_db.cjs
+++ src/services/settings_db.cjs
@@ -2,7 +2,7 @@
 const { get } = require('./poolManagement.cjs')
 
 module.exports = {
-  getMode: async function getMode (username) {
+  getMode: async function getMode(username) {
     // Get dark mode setting for specific user
     const modeRequest = `SELECT isDarkmode FROM Settings WHERE Username = '${username}'`
     return new Promise((resolve, reject) => {
@@ -17,15 +17,16 @@ module.exports = {
   },
 
   // This should go in its own file, not in testDB which is for testing the DB and pool.
-  changeMode: async function changeMode (DarkMode, username) {
+  changeMode: async function changeMode(DarkMode, username) {
     // update users table if dark mode has changed
     const sqlCode = `UPDATE Settings SET isDarkmode = '${DarkMode}' WHERE Username = '${username}'`
-
-    console.log(sqlCode)
-    get('default').then(
-      (pool) => pool.request().query(sqlCode).then(
-        (result) => { return result }
+    return new Promise((resolve, reject) => {
+      console.log(sqlCode)
+      get('default').then(
+        (pool) => pool.request().query(sqlCode).then(
+          (result) => { return result }
+        ).catch(console.error)
       ).catch(console.error)
-    ).catch(console.error)
+    })
   }
 }
